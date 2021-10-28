dc4c9d2d1a548b617a75b28548933a4a7d958879
Jeff Zhan <jeffzhan2008@gmail.com>
2021-10-28 19:35:56 -0400

    added function to grab comments from submission object

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -36,7 +36,6 @@ class Crawler:
         -------
         List of submissions
         """
-        headlines = [] 
         start_epoch = int(start.timestamp())
         submissions = list(self.api.search_submissions(after=start_epoch,
                                                        subreddit=sub,
@@ -44,3 +43,22 @@ class Crawler:
 
         # submission follows https://praw.readthedocs.io/en/stable/code_overview/models/submission.html
         return submissions
+
+    def get_comments(self, submission, threshold):
+        """
+        Gets Comments from a praw submission above a threshold
+
+        Parameters
+        ----------
+        submission: submission
+            praw submission object
+        threshold: int 
+            top n comments to return 
+
+        Returns
+        -------
+        List of Comments
+        """
+        submission.comment_sort = "top"
+        comments = list(submission)
+        return comments[0:threshold] if threshold < len(comments) else comments
