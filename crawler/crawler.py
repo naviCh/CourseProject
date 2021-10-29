0f2c5c5744083cfa8414ba3020ff2b6e7847b6b1
Jeff Zhan <jeffzhan2008@gmail.com>
2021-10-28 21:36:54 -0400

    fix comments and add sort submissions based on votecount

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,4 +1,5 @@
 import praw 
+from praw.models import MoreComments
 from psaw import PushshiftAPI
 from pathlib import Path
 import datetime as dt
@@ -20,6 +21,7 @@ class Crawler:
                                 )
         self.api = PushshiftAPI(self.reddit)
     
+   
     def crawl(self, sub, start, end, upperlimit):
         """
         Crawls subreddit and finds headlines between two dates 
@@ -36,7 +38,7 @@ class Crawler:
             Limit of submissions
         Returns
         -------
-        List of submissions
+        List of submission objects
         """
         start_epoch = int(start.timestamp())
         end_epoch = int(end.timestamp())
@@ -44,10 +46,27 @@ class Crawler:
                                                        before=end_epoch,
                                                        subreddit=sub,
                                                        limit=upperlimit))
-
         # submission follows https://praw.readthedocs.io/en/stable/code_overview/models/submission.html
         return submissions
 
+
+    def sort_format_submissions(self, submissions):
+        reformated = [] 
+        for submission in submissions: 
+            entry = {} 
+            entry['title'] = submission.title
+            entry['url'] = submission.url
+            post_date = dt.datetime.utcfromtimestamp(submission.created_utc)
+            post_date_str = post_date.strftime("%m/%d/%Y")
+            entry['postdate'] = post_date_str
+            entry['upvotes'] = submission.score 
+            downvotes = submission.score / submission.upvote_ratio - submission.score
+            entry['downvotes'] = int(downvotes)
+            reformated.append(entry)
+        sorted_submissions = sorted(reformated, key=lambda x: x['upvotes'], reverse=True)
+        return sorted_submissions
+        
+    
     def get_comments(self, submission, threshold):
         """
         Gets Comments from a praw submission above a threshold
@@ -61,8 +80,11 @@ class Crawler:
 
         Returns
         -------
-        List of Comments
+        List of Comments in Markdown
         """
         submission.comment_sort = "top"
-        comments = submission.comments.list()
+        submission.comments.replace_more(limit=None)
+        comments = submission.comments
         return comments[0:threshold] if threshold < len(comments) else comments
+
+    
